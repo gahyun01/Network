37a144ba12606c82bb2663c30680575e79802a44
정가현 <135098077+gahyun01@users.noreply.github.com>
2024-03-08 11:18:16 +0900

    Add files via upload

---
+++ 20231101_202184050_정가현_MultiThreadTCPServer/202184050_정가현_MultiThreadTCPServer.cpp
@@ -0,0 +1,112 @@
+#include "common.h"
+
+#define SERVERPORT 9000
+#define BUFSIZE 512
+
+// 클라이언트와 데이터 통신
+DWORD WINAPI ProcessClient(LPVOID arg)
+{
+	SOCKET client_sock = (SOCKET)arg;
+	int retval;
+	struct sockaddr_in clientaddr;
+	char addr[INET_ADDRSTRLEN];
+	int addrlen;
+	char buf[BUFSIZE + 1];
+
+	// 클라이언트 정보 얻기
+	addrlen = sizeof(clientaddr);
+	getpeername(client_sock, (struct sockaddr*)&clientaddr, &addrlen);
+	inet_ntop(AF_INET, &clientaddr.sin_addr, addr, sizeof(addr));
+
+	while (1) {
+		// 데이터 받기
+		retval = recv(client_sock, buf, BUFSIZE, 0);
+		if (retval == SOCKET_ERROR) {
+			err_display("recv()");
+			break;
+		}
+		else if (retval == 0)
+			break;
+
+		// 받은 데이터 출력
+		buf[retval] = '\0';
+		printf("[ TCP / %s:%d ] %s\n", addr, ntohs(clientaddr.sin_port), buf);
+
+		// 데이터 보내기
+		retval = send(client_sock, buf, retval, 0);
+		if (retval == SOCKET_ERROR) {
+			err_display("send()");
+			break;
+		}
+	}
+
+	// 소켓 닫기
+	closesocket(client_sock);
+	printf("[ TCP 서버 ] 클라이언트 종료 : IP 주소 = %s, 포트번호 = %d\n", addr, ntohs(clientaddr.sin_port));
+	return 0;
+}
+
+int main(int argc, char* argv[]) {
+	int retval;		//소켓 관련 변수
+
+	// 윈속 초기화
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+		return 1;
+
+	// 소켓 생성
+	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (listen_sock == INVALID_SOCKET) 
+		err_quit("socket()");
+
+	// bind()
+	struct sockaddr_in serveraddr;
+	memset(&serveraddr, 0, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons(SERVERPORT);
+	retval = bind(listen_sock, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+	if (retval == SOCKET_ERROR) 
+		err_quit("bind()");
+
+	// 이제부터 어세스 허용해야함
+	// listen()
+	retval = listen(listen_sock, SOMAXCONN);	// SOMAXCONN 에 정수값 넣어도 상관없음
+	if (retval == SOCKET_ERROR) 
+		err_quit("listen()");
+
+	// 데이터 통신에 사용할 변수
+	SOCKET client_sock;
+	struct sockaddr_in clientaddr;
+	int addrlen;
+	HANDLE hThread;
+
+	while (1) {		// 계속 접속해서 무한반복함
+		// accept()
+		addrlen = sizeof(clientaddr);	// 길이값 지정
+		client_sock = accept(listen_sock, (struct sockaddr*)&clientaddr, &addrlen);	// 나의 서버 클라이언트 지정
+		if (client_sock == INVALID_SOCKET) {
+			err_display("accept()");
+			break;
+		}	// 외부에서 접속이 이루어 졌을때 실행
+
+		// 접속한 클라이언트 정보 출력
+		char addr[INET_ADDRSTRLEN];
+		inet_ntop(AF_INET, &clientaddr.sin_addr, addr, sizeof(addr));
+		printf("\n[TCP 서버] 클라이언트 접속 : IP 주소 = %s, 포트 번호 = %d\n",
+			addr, ntohs(clientaddr.sin_port));
+		// 원래 무한반복 들어감
+
+		// 스레드 생성
+		hThread = CreateThread(NULL, 0, ProcessClient, (LPVOID)client_sock, 0, NULL);
+		if (hThread == NULL) { closesocket(client_sock); }
+		else { CloseHandle(hThread); }
+	}
+
+	// 소켓 닫기
+	closesocket(listen_sock);
+
+	// 윈속 종료
+	WSACleanup();
+	return 0;
+}
